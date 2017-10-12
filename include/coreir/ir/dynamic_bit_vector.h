64f7b86c9c5aab46d7d80da0a5373c19f28ec3c8
Lenny Truong <leonardtruong@protonmail.com>
2017-10-12 16:10:57 -0700

    Try using bits.size() instead of N

--- include/coreir/ir/dynamic_bit_vector.h
+++ include/coreir/ir/dynamic_bit_vector.h
@@ -76,11 +76,11 @@ namespace bsim {
     	return *this;
       }
 
+      N = other.bitLength();
       bits.resize(other.bits.size());
 
-      N = other.bitLength();
-      for (int i = 0; i < NUM_BYTES(N); i++) {
-        bits[i] = other.bits[i];
+      for (int i = 0; i < other.bits.size(); i++) {
+        set(i, other.get(i));
       }
 
 
